6410054c347d90a94fd47e983b19bb1f27eb5973
VivekRajpara125 <vivek.rajpara@rapidops.com>
2023-03-28 10:09:14 +0530

    email change

--- server/src/controllers/send-mail.controller.js
+++ server/src/controllers/send-mail.controller.js
@@ -8,13 +8,13 @@ const sendEmail = async (email, subject, url) => {
 			port: Number(process.env.EMAIL_PORT),
 			secureConnection: Boolean(process.env.SECURE),
 			auth: {
-				user: process.env.USEREMAILID,
+				user: process.env.USEREMAILID + process.env.DOMAIN,
 				pass: process.env.PASS,
 			},
 		});
 
 		await transporter.sendMail({
-			from: process.env.USEREMAILID,
+			from: process.env.USEREMAILID + process.env.DOMAIN,
 			to: email,
 			subject: subject,
 			text:`Hellow folk! \n  for Activate your Account click here :- ${url}` ,
